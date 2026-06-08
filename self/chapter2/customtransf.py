e303fb607644adfb57feaa97971379a5c425386c
vanshaj <vanshajdhar@gmail.com>
2026-06-08 08:03:28 +0530

    Added non working custom trasnformer pipeline

--- self/chapter2/customtransf.py
+++ self/chapter2/customtransf.py
@@ -1,5 +1,6 @@
-
+import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import MultiLabelBinarizer
 
 
 class CustomTransf(BaseEstimator, TransformerMixin):
@@ -16,3 +17,23 @@ class CustomTransf(BaseEstimator, TransformerMixin):
         X["derived"] = X[self.numer] / X[self.denom]
         return X
 
+class CustomMLBEncoding(BaseEstimator, TransformerMixin):
+    
+    def __init__(self, column):
+        self.column = column
+
+    def fit(self, X, y= None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        mlb = MultiLabelBinarizer()
+        values = X[self.column].str.split(",")
+        values_df = pd.DataFrame(
+            mlb.fit_transform(values),
+            columns=mlb.classes_,
+            index=X.index
+        )
+        X.drop(columns=[self.column], inplace=True)
+        X = pd.concat([X, values_df], axis=1)
+        return X
--- self/chapter2/main.py
+++ self/chapter2/main.py
@@ -1,6 +1,11 @@
 import os
 
+import numpy as np
+import pandas as pd
+
+from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
 
 from load import load, unload
 from binn import binn, split
@@ -9,6 +14,27 @@ from cleanup import clean, one_hot_encoding
 from sklearn.pipeline import Pipeline
 import customtransf
 
+def create_pipeline(df: pd.DataFrame):
+    numreical_steps = [
+        ('cleanup', SimpleImputer(missing_values=np.nan, strategy="mean")),
+        ('custom', customtransf.CustomTransf("Value", "Wage")),
+        ('scaler', StandardScaler())
+    ]
+    category_steps = [
+        ('cleanup', SimpleImputer(strategy="most_frequent")),
+        ('encoding', customtransf.CustomMLBEncoding("Positions Played"))
+    ]
+
+    num_attribs = list(df.select_dtypes(include=['int64', 'float64']).columns)
+    cat_attribs = list(df.select_dtypes(include=['object']).columns)
+
+    full_pipeline = ColumnTransformer([
+        # ("num", numreical_steps, num_attribs),
+        ("cat", category_steps, cat_attribs),
+    ])
+
+    df_transformed = full_pipeline.fit_transform(df)
+    return df_transformed
 
 def main():
     folder = os.getcwd()
@@ -23,10 +49,12 @@ def main():
     # scatterPlot(train_set, "Potential", "Value")
     # correlation(train_set, ["Value", "Overall", "Potential", "Wage"])
     # clean(df)
-    df_new = one_hot_encoding(df, "Positions Played")
-    val_per_wage_t = customtransf.CustomTransf("Value", "Wage")
-    df_new = val_per_wage_t.fit_transform(df_new)
-    unload(df_new, new_filepath)
+    # df_new = one_hot_encoding(df, "Positions Played")
+    # val_per_wage_t = customtransf.CustomTransf("Value", "Wage")
+    # df_new = val_per_wage_t.fit_transform(df_new)
+    train_set_clean = train_set.drop("Value", axis = 1) # We dropped the Value as it will be our Y that we need to predict
+    transformed_train_set = create_pipeline(train_set_clean)
+    unload(transformed_train_set, new_filepath)
 
 
 
