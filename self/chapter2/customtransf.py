c332710ddfb8d02d4a89fa361f5749ca52c1f806
vanshajdhar <vanshaj.dhar@aquasec.com>
2026-06-08 20:11:09 +0530

    Successfully coded custom transformer that will apply multilabel encoding on the string data

--- self/chapter2/customtransf.py
+++ self/chapter2/customtransf.py
@@ -1,15 +1,15 @@
 import pandas as pd
+import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import MultiLabelBinarizer
 
 
 class CustomTransf(BaseEstimator, TransformerMixin):
-
     def __init__(self, columnA, columnB):
         self.columnA = columnA
         self.columnB = columnB
 
-    def fit(self, X, y = None):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X):
@@ -17,23 +17,41 @@ class CustomTransf(BaseEstimator, TransformerMixin):
         X["derived"] = X[self.columnA] / X[self.columnB]
         return X
 
-class CustomMLBEncoding(BaseEstimator, TransformerMixin):
-    
-    def __init__(self, column):
-        self.column = column
 
-    def fit(self, X, y= None):
+class CustomMLBEncoding(BaseEstimator, TransformerMixin):
+    def __init__(self, column_index):
+        self.column_index = column_index
+        self.classes = None
+
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X):
+        # Copy nd array
         X = X.copy()
+        # Create multilabel binarizer
         mlb = MultiLabelBinarizer()
-        values = X[self.column].str.split(",")
-        values_df = pd.DataFrame(
-            mlb.fit_transform(values),
-            columns=mlb.classes_,
-            index=X.index
-        )
-        X.drop(columns=[self.column], inplace=True)
-        X = pd.concat([X, values_df], axis=1)
+        # Get data from the target column i.e "RW,ST,CB"
+        target_column = X[:, self.column_index]
+        """
+        Now for fit_transform we need 2d array [[], [], ...] like this
+        so we iterate over each row and split with , and then append each array as a row in 2d array
+        then applied mlb
+        """
+        split_columns = [row.split(',') for row in target_column]
+        new_multiple_columns = mlb.fit_transform(split_columns)
+        # We want the column passed to be removed from our dataset
+        X = np.delete(X, self.column_index, axis=1)
+        # Store the classes generated from the mlb as it will be used as new columns in the data when create df
+        self.classes = mlb.classes_
+        # Concat the new data generated after transformation in the exiting data and return
+        X = np.concatenate((X, new_multiple_columns), axis=1)
         return X
+        """
+        Below code is used if you are getting dataframe in X rather than np.ndarray
+            values_df = pd.DataFrame(
+                mlb.fit_transform(split_columns), columns=mlb.classes_, index=X.index
+            )
+            X.drop(columns=[self.column], inplace=True)
+            X = pd.concat([X, values_df], axis=1)
+        """
--- self/chapter2/main.py
+++ self/chapter2/main.py
@@ -14,51 +14,38 @@ from cleanup import clean, one_hot_encoding
 from sklearn.pipeline import Pipeline
 import customtransf
 
-def create_pipeline(df: pd.DataFrame):
-    num_attribs = list(df.select_dtypes(include=['int64', 'float64']).columns)
-    cat_attribs = list(df.select_dtypes(include=['object']).columns)
-
-    index_of_value = num_attribs.index("Value")
-    index_of_wage = num_attribs.index("Wage")
-
-    index_of_positions_played = cat_attribs.index("Positions Played")
-
-    numreical_steps = [
-        ('cleanup', SimpleImputer(missing_values=np.nan, strategy="mean")),
-        ('custom', customtransf.CustomTransf("Value", "Wage")),
-        ('scaler', StandardScaler())
-    ]
+def create_pipeline(df: pd.DataFrame, columnName: str):
+    # Get  index of the column
+    index_of_positions_played = df.columns.get_loc(columnName)
+    # Create custom transformer based on the index of that column, why because in np.ndarray column names will not work
+    custTransf = customtransf.CustomMLBEncoding(index_of_positions_played)
+    # Create a simple pipeline that will use SimpleImputer and then custom transformer
     category_steps_pip = Pipeline([
-        # ('cleanup', SimpleImputer(strategy="most_frequent")),
-        ('encoding', customtransf.CustomMLBEncoding(index_of_positions_played))
+        ('cleanup', SimpleImputer(strategy="most_frequent")),
+        ('encoding', custTransf),
     ])
-    df_transformed = category_steps_pip.fit_transform(df)
-    """
-    full_pipeline = ColumnTransformer([
-        ("num", Pipeline(numreical_steps), num_attribs),
-        ("cat", category_steps_pip, cat_attribs),
-    ])
-    df_transformed = full_pipeline.fit_transform(df)
-    """
+    # Apply transformation to get the numpy ndarray
+    np_transformed = category_steps_pip.fit_transform(df)
+    # Drop the original column from the actual data frame so that we can append new columns
+    df_without_column = df.drop(columns=[columnName])
+    # Get all the existing columns
+    existing_columns = df_without_column.columns.values
+    # Create an array of existing columns with the new transformed columns that was being set as a variable in customTransf class object
+    transformed_columns = np.append(existing_columns, custTransf.classes)
+    # Create the dataframe from the transformed numpy ndarray
+    df_transformed = pd.DataFrame(np_transformed, columns=transformed_columns, index=df_without_column.index)
 
     return df_transformed
 
 def main():
     folder = os.getcwd()
-    filename = "data/Fifa23Players.csv"
-    new_filename = "data/Fifa23PlayersTransformed.csv"
+    filename = "self/data/Fifa23Players.csv"
+    new_filename = "self/data/Fifa23PlayersTransformed.csv"
     filepath = os.path.abspath(f"{folder}/{filename}")
     new_filepath = os.path.abspath(f"{folder}/{new_filename}")
     df = load(filepath)
     df = binn(df)
     train_set, test_set = split(df)
-    # histogram(train_set)
-    # scatterPlot(train_set, "Potential", "Value")
-    # correlation(train_set, ["Value", "Overall", "Potential", "Wage"])
-    # clean(df)
-    # df_new = one_hot_encoding(df, "Positions Played")
-    # val_per_wage_t = customtransf.CustomTransf("Value", "Wage")
-    # df_new = val_per_wage_t.fit_transform(df_new)
     y_train = train_set["Value"]
     transformed_train_set = create_pipeline(train_set)
     unload(transformed_train_set, new_filepath)
